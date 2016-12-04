dcb84a251c04833a20a6007572fbb17dfa369adf
sevenvoid <sevenvoid.luo@gmail.com>
2016-12-04 22:26:55 +0800

    thread

--- src/edu/sevenvoid/jvm/thread/SynchronizedTest.java
+++ src/edu/sevenvoid/jvm/thread/SynchronizedTest.java
@@ -12,54 +12,25 @@ public class SynchronizedTest implements Runnable{
 	
 	private long i = 0;
 	
-	private static long j = 0;
-	
 	public synchronized long inCrementWithSynchronizedMethod() {
 		i++;
 		System.out.println("The thread name : " + Thread.currentThread().getName() + "  i = " + i);
 		return i;
 	}
 	
-	public long inCrementWithSynchronizedBlock(Object obj) {
-		long ii = i;
-		synchronized (obj) {
-			i++;
-			System.out.println("The thread name : " + Thread.currentThread().getName() + "  i = " + i);
-			ii = i;
-		}
-		return ii;
-	}
-	
-	public synchronized static long inCrementWithSynchronizedStatic() {
-		j++;
-		System.out.println("The thread name : " + Thread.currentThread().getName() + "  j = " + j);
-		return j;
-	}
-	
 	@Override
 	public void run() {
-		for(int i=0; i<5; i++) {
-			inCrementWithSynchronizedStatic();
+		for(int i=0; i<2; i++) {
+			inCrementWithSynchronizedMethod();
 		}
 	}
 	
 	public static void main(String arg[]) {
-		SynchronizedTest synchronizedTest = new SynchronizedTest();
-		
-//		Thread thread1 = new Thread(synchronizedTest);
-//		Thread thread2 = new Thread(synchronizedTest);
-//		
-//		thread1.start();
-//		thread2.start();
-		
-//		SynchronizedTest synchronizedTest1 = new SynchronizedTest();
-		
-		Thread thread3 = new Thread(synchronizedTest);
-		Thread thread4 = new Thread(synchronizedTest);
-		
-		thread3.start();
-		thread4.start();
+		SynchronizedTest synchronizedTest1 = new SynchronizedTest();
+		SynchronizedTest synchronizedTest2 = new SynchronizedTest();
+		Thread thread1 = new Thread(synchronizedTest1);
+		Thread thread2 = new Thread(synchronizedTest2);
+		thread1.start();
+		thread2.start();
 	}
-
-
 }
